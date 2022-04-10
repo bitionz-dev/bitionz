5659379990d791f6373f8aac9b4f0764bb9f3da0
gastonivanfrasca <gastonivanfrasca@hotmail.com>
2022-04-10 04:50:50 -0300

    more pointers

--- components/Shared/FullCard/FullCard.js
+++ components/Shared/FullCard/FullCard.js
@@ -4,8 +4,8 @@ import {Card} from "react-bootstrap";
 export default function FullCard({title, text, imgURL, altText, onClick}) {
     return (
         <Card className="bg-dark text-white" onClick={onClick} style={{cursor: 'pointer'}}>
-            <Card.Img src={imgURL} alt={altText}/>
-            <Card.ImgOverlay>
+            <Card.Img src={imgURL} alt={altText} onClick={onClick} style={{cursor: 'pointer'}}/>
+            <Card.ImgOverlay onClick={onClick} style={{cursor: 'pointer'}}>
                 <Card.Title>{title}</Card.Title>
                 <Card.Text>
                     {text}
