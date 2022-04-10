c050fc4aeeaeb1dc94281b11e452488f35ac34fb
gastonivanfrasca <gastonivanfrasca@hotmail.com>
2022-04-10 05:02:31 -0300

    added mobile div

--- components/Shared/FullCard/FullCard.js
+++ components/Shared/FullCard/FullCard.js
@@ -3,14 +3,18 @@ import {Card} from "react-bootstrap";
 
 export default function FullCard({title, text, imgURL, altText, onClick, onTouchStart}) {
     return (
-        <Card className="bg-dark text-white" onClick={onClick} style={{cursor: 'pointer'}} onTouchStart={onTouchStart}>
-            <Card.Img src={imgURL} alt={altText}/>
-            <Card.ImgOverlay>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>
-                    {text}
-                </Card.Text>
-            </Card.ImgOverlay>
-        </Card>
+        <div onClick={onClick} style={{cursor: 'pointer'}}
+             onTouchStart={onTouchStart}>
+            <Card className="bg-dark text-white" onClick={onClick} style={{cursor: 'pointer'}}
+                  onTouchStart={onTouchStart}>
+                <Card.Img src={imgURL} alt={altText}/>
+                <Card.ImgOverlay>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        {text}
+                    </Card.Text>
+                </Card.ImgOverlay>
+            </Card>
+        </div>
     );
 }
