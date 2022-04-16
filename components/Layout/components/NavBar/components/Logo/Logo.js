eaa450666eb53a3419e6e4ef2ac9d3cabe506d81
gastonivanfrasca <gastonivanfrasca@hotmail.com>
2022-04-16 12:57:21 -0300

    fixed more laptop styles

--- components/Layout/components/NavBar/components/Logo/Logo.js
+++ components/Layout/components/NavBar/components/Logo/Logo.js
@@ -3,6 +3,7 @@ import DesktopSvg from "./DesktopSvg";
 import MobileSvg from "./MobileSvg";
 import styles from "./Logo.module.css"
 import {useRouter} from 'next/router'
+import LaptopSvg from "./LaptopSvg";
 
 const Logo = () => {
     const router = useRouter()
@@ -10,6 +11,7 @@ const Logo = () => {
         <>
             <DesktopSvg className={styles.desktop} onClick={() => router.push(`/`)}/>
             <MobileSvg className={styles.mobile} onClick={() => router.push(`/`)}/>
+            <LaptopSvg className={styles.laptop} onClick={() => router.push(`/`)}/>
         </>
     )
 }
