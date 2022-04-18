142107f7cc02e93b7fcf437eb3da71c8eff9b32f
gastonivanfrasca <gastonivanfrasca@hotmail.com>
2022-04-18 17:50:09 -0300

    fit content

--- components/Shared/LargeImage/LargeImage.js
+++ components/Shared/LargeImage/LargeImage.js
@@ -4,7 +4,7 @@ import styles from './LargeImage.module.css'
 
 export default function LargeImage({imgURL, altText, suggested}) {
     return (
-        <Card className={!suggested && styles.container}>
+        <Card className={!suggested ? styles.container : styles.suggestedContainer}>
             <Card.Img src={imgURL} alt={altText} className={suggested ? styles.image : styles.imageCommon}/>
         </Card>
     );
