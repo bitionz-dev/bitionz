2aa879283f51e4851d66e25d465b0679e1f86d92
gastonivanfrasca <gastonivanfrasca@hotmail.com>
2022-05-01 22:13:04 -0300

    added scripts id

--- components/Tools/Page/Page.js
+++ components/Tools/Page/Page.js
@@ -81,8 +81,8 @@ export default function Page({title, meta, bootstrap, email = true}) {
             {bootstrap &&
                 <Script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js" crossOrigin></Script>}
             {email && <Script src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></Script>}
-            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-TXK9TGRKPG"></Script>
-            <Script>
+            <Script id="analytics-script" async src="https://www.googletagmanager.com/gtag/js?id=G-TXK9TGRKPG"></Script>
+            <Script id="analytics-script2">
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments)}
                 gtag('js', new Date());
