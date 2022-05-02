34db6ed4c4228ba590a6b9b8a38fb50963964a93
gastonivanfrasca <gastonivanfrasca@hotmail.com>
2022-05-01 22:42:02 -0300

    Bugsnag integration

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -2,13 +2,22 @@ import NavBar from "./components/NavBar/NavBar";
 import {LayoutContext} from "../Tools/Context/Context"
 import {useEffect, useState} from "react";
 import {useMediaQuery} from "react-responsive";
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginReact from '@bugsnag/plugin-react'
 import LowerSearchSection from "./components/LowerSearchSection/LowerSearchSection";
 import FilterBar from "./components/FilterBar/FilterBar";
 import Footer from "../Shared/Footer/Footer";
 import DrawerMenu from "./components/Shared/DrawerMenu/DrawerMenu";
 import Page from "../Tools/Page/Page";
 
+
 export default function Layout({children, tokens}) {
+    Bugsnag.start({
+        apiKey: '5b72d4f682e56ccf804edbefc1d52696',
+        plugins: [new BugsnagPluginReact()]
+    })
+    const ErrorBoundary = Bugsnag.getPlugin('react')
+        .createErrorBoundary(React)
     const isTabletOrMobile = useMediaQuery({maxWidth: 1224})
     const [showLowerSearchSection, setShowLowerSearchSection] = useState(false);
     const [showFilters, setShowFilters] = useState(false);
@@ -27,40 +36,43 @@ export default function Layout({children, tokens}) {
         bootstrap: true,
         email: true
     }
+    Bugsnag.notify(new Error('Test error'))
     useEffect(() => {
         setFilteredTokens(tokens)
     }, [tokens])
     return (
-        <LayoutContext.Provider value={{
-            setShowLowerSearchSection: setShowLowerSearchSection,
-            showLowerSearchSection: showLowerSearchSection,
-            showFilters: showFilters,
-            setShowFilters: setShowFilters,
-            showDrawerMenu: showDrawerMenu,
-            setShowDrawerMenu: setShowDrawerMenu,
-            filters: {
-                blockchains: blockchainsFilter,
-                setBlockchains: setBlockchainsFilter,
-                types: typesFilter,
-                setTypes: setTypesFilter,
-                nft: nftFilter,
-                setNft: setNftFilter
-            },
-            tokens: tokens,
-            setFilteredTokens: setFilteredTokens,
-            filteredTokens: filteredTokens,
-            lang: lang,
-            setLang: setLang
-        }}>
-            <Page title={page.title} meta={page.meta} bootstrap={page.bootstrap}/>
-            <NavBar/>
-            {showDrawerMenu && <DrawerMenu/>}
-            {showFilters && <FilterBar/>}
-            {showLowerSearchSection && isTabletOrMobile && <LowerSearchSection/>}
-            <hr className={"navBarLine"}/>
-            <main>{children}</main>
-            <hr className={"navBarLineFooter"}/>
-            <Footer/>
-        </LayoutContext.Provider>
+        <ErrorBoundary>
+            <LayoutContext.Provider value={{
+                setShowLowerSearchSection: setShowLowerSearchSection,
+                showLowerSearchSection: showLowerSearchSection,
+                showFilters: showFilters,
+                setShowFilters: setShowFilters,
+                showDrawerMenu: showDrawerMenu,
+                setShowDrawerMenu: setShowDrawerMenu,
+                filters: {
+                    blockchains: blockchainsFilter,
+                    setBlockchains: setBlockchainsFilter,
+                    types: typesFilter,
+                    setTypes: setTypesFilter,
+                    nft: nftFilter,
+                    setNft: setNftFilter
+                },
+                tokens: tokens,
+                setFilteredTokens: setFilteredTokens,
+                filteredTokens: filteredTokens,
+                lang: lang,
+                setLang: setLang
+            }}>
+                <Page title={page.title} meta={page.meta} bootstrap={page.bootstrap}/>
+                <NavBar/>
+                {showDrawerMenu && <DrawerMenu/>}
+                {showFilters && <FilterBar/>}
+                {showLowerSearchSection && isTabletOrMobile && <LowerSearchSection/>}
+                <hr className={"navBarLine"}/>
+                <main>{children}</main>
+                <hr className={"navBarLineFooter"}/>
+                <Footer/>
+            </LayoutContext.Provider>
+        </ErrorBoundary>
     )
 }
